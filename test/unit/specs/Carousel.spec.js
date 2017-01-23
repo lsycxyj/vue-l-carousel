240ea66b1a4ffb0f93bbbe66130c7b9c1844cff1
Light <lsycxyj@gmail.com>
2017-01-23 15:55:22 +0800

    init test. But there could be a much better way.

--- test/unit/specs/Carousel.spec.js
+++ test/unit/specs/Carousel.spec.js
@@ -1,8 +1,66 @@
 import Vue from 'vue';
-import {Carousel, CarouselItem} from '../../../src/index'
+import $ from '../../../src/util';
+import {Carousel, CarouselItem} from '../../../src/index';
 
-describe('test hello', () => {
+describe('Suite: test Carousel.vue', () => {
+
+	//beforeEach(() => {
+	//});
+	//
+	//afterEach(() => {
+	//});
+
 	it('test hello', () => {
-		expect(true).toEqual(true);
+		let vm,
+			elApp = document.createElement('div'),
+			elBody = document.body;
+
+		elApp.id = 'app';
+		$.append(elBody, elApp);
+
+		vm = new Vue({
+			el: '#app',
+			template: `
+				<div>
+					<carousel :watchItems="list">
+						<carousel-item v-for="(item, index) in list">
+							<p>CarouselItem{{index}}, URL is {{item.url}}</p>
+						</carousel-item>
+						<div slot="before">Insert node before</div>
+						<div slot="after">Insert node after</div>
+					</carousel>
+				</div>
+			`,
+			mounted() {
+				//elApp outside will be replaced.
+				const elApp = this.$el;
+
+				expect(true).toEqual(true);
+			},
+			data() {
+				return {
+					auto: 0,
+					list: [
+						{
+							url: 'url1'
+						},
+						{
+							url: 'url2'
+						},
+						{
+							url: 'url3'
+						}
+					],
+					speed: 300,
+					loop: true,
+					rewind: false
+				};
+			},
+			components: {
+				'carousel': Carousel,
+				'carousel-item': CarouselItem
+			}
+		});
+
 	});
 });
